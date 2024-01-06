2deac35d41d8024f29f742b65d5c4bf701bc61c7
Nursultan Akhmetzhanov <nursultan.akhmetzhanov@codemate.com>
2024-01-06 18:15:27 +0200

    chore: installed several libraries and set up eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,5 +14,16 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    indent: ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "single"],
+      "no-console": "warn",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { vars: "all", args: "after-used", ignoreRestSiblings: false },
+      ],
+      "@typescript-eslint/explicit-function-return-type": "warn", // Consider using explicit annotations for object literals and function return types even when they can be inferred.
+      "no-empty": "warn",
   },
 }
